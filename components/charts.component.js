a76d5696104cf4dcdd47844d75b59da26c38427a
Boopathi Krishnan <boopathi.career@live.com>
2019-02-28 15:59:43 +0530

    Update charts.component.js
    added bar chart

--- components/charts.component.js
+++ components/charts.component.js
@@ -3,6 +3,7 @@ import {LineChart ,Line, CartesianGrid,XAxis,YAxis} from 'recharts';
 import {BarChart, Bar} from 'recharts';
 
 const data =[{name:'Boopathi', age:28},{name:'Krishnan', age:48},{name:'Dinesh', age:25},{name:'Rajesh', age:30}];
+const data1 =[{name:'Boopathi', salary:50000},{name:'Krishnan', salary:30000},{name:'Dinesh', salary:700000},{name:'Rajesh', salary:100000}];
 
 class ChartComp extends React.Component{
     
@@ -15,7 +16,13 @@ class ChartComp extends React.Component{
                         <CartesianGrid stroke="#ccc"/>
                         <XAxis dataKey="name"/>
                         <YAxis/>
-                    </LineChart> 
+                    </LineChart>
+                    <BarChart width={600} height={300} data={data1}>
+                        <Bar type="monotone" dataKey="salary" fill="#8884a8" barSize={100}/>
+                        <CartesianGrid stroke="#ccc"/>
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                    </BarChart> 
             </div>
         )
     }
