12481b298d726bcf3d571822d260795150e644b7
Boopathi Krishnan <boopathi.career@live.com>
2019-03-01 13:54:45 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import Home from './components/home.component';
-import Aboutus from './components/register.component';
+import Register from './components/register.component';
 import Contactus from './components/contactus.component';
 import Students from './components/students.component';
 import ChartComp from './components/charts.component';
@@ -13,7 +13,7 @@ class App extends React.Component {
             <nav className="navbar navbar-dark bg-warning">
                <ul className="nav navbar-nav">
                   <li><Link to="/">Home</Link></li>
-                  <li><Link to="/aboutus">Register</Link></li>
+                  <li><Link to="/register">Register</Link></li>
                   <li><Link to="/contactus">Contact Us</Link></li>
                   <li><Link to="/students">Students</Link></li>
                   <li><Link to="/charts">Charts</Link></li>
@@ -22,7 +22,7 @@ class App extends React.Component {
 
             <Switch>
                <Route exact path="/" component={Home} />
-               <Route exact path="/aboutus" component={Aboutus} />
+               <Route exact path="/register" component={Register} />
                <Route exact path="/contactus" component={Contactus} />
                <Route exact path="/students" component={Students} />
                <Route exact path="/charts" component={ChartComp} />
